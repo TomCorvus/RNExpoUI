623323099966d77ad5ca2709dfe1511c8fe00ec4
Thomas Corbin <thomas@comptalib.com>
2026-06-12 10:52:29 +0200

    update @expo/ui

--- App.js
+++ App.js
@@ -1,36 +1,73 @@
-import { useRef } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import PagerView from '@expo/ui/community/pager-view';
+import { useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import PagerView from "@expo/ui/community/pager-view";
 
 export default function App() {
-   const pagerRef = useRef(null);
+  const pagerRef = useRef(null);
+
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   return (
-     <View style={{ flex: 1, padding: 50 }}>
+    <View style={{ flex: 1, padding: 50 }}>
       <PagerView
         ref={pagerRef}
         scrollEnabled={false}
         style={{ flex: 1 }}
         initialPage={0}
-        onPageSelected={event => {
-          console.log('selected page', event.nativeEvent.position);
-        }}>
-        <View key="one" style={[styles.page, { backgroundColor: '#fde68a' }]}>
-            <Text>Page one</Text>
-            <Button title="Go to page 2" onPress={() => pagerRef.current?.setPage(1)} />
-            <Button title="Go to page 3" onPress={() => pagerRef.current?.setPage(2)} />
+        onPageSelected={(event) => {
+          console.log("selected page", event.nativeEvent.position);
+        }}
+      >
+        <View key="one" style={styles.page}>
+          <Text style={{ margin: 10 }}>Page one</Text>
+          <Button
+            title="Click me"
+            onPress={() => {
+              alert("clicked");
+            }}
+            style={{ padding: 10 }}
+          />
         </View>
-        <View key="two" style={[styles.page, { backgroundColor: '#bfdbfe' }]}>
-            <Text>Page two</Text>
-            <Button title="Go to page 1" onPress={() => pagerRef.current?.setPage(0)} />
-            <Button title="Go to page 3" onPress={() => pagerRef.current?.setPage(2)} />
+        <View key="two" style={styles.page}>
+          <Text style={{ margin: 10 }}>Page two</Text>
+          <Button
+            title="Click me"
+            onPress={() => {
+              alert("clicked");
+            }}
+            style={{ padding: 10 }}
+          />
         </View>
-        <View key="three" style={[styles.page, { backgroundColor: '#bbf7d0' }]}>
-            <Text>Page three</Text>
-            <Button title="Go to page 1" onPress={() => pagerRef.current?.setPage(0)} />
-            <Button title="Go to page 2" onPress={() => pagerRef.current?.setPage(1)} />
+        <View key="three" style={styles.page}>
+          <Text style={{ margin: 10 }}>Page three</Text>
+          <Button
+            title="Click me"
+            onPress={() => {
+              alert("clicked");
+            }}
+            style={{ padding: 10 }}
+          />
         </View>
       </PagerView>
+      <View style={{ flexDirection: "row" }}>
+        <Button
+          title="Previous"
+          onPress={() => {
+            setCurrentSlide(currentSlide - 1);
+            pagerRef.current?.setPage(currentSlide - 1);
+          }}
+          style={{ padding: 10 }}
+        />
+
+        <Button
+          title="Next"
+          onPress={() => {
+            setCurrentSlide(currentSlide + 1);
+            pagerRef.current?.setPage(currentSlide + 1);
+          }}
+          style={{ padding: 10 }}
+        />
+      </View>
     </View>
   );
 }
@@ -38,8 +75,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
